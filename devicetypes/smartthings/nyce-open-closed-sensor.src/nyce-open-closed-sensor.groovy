b5137eacf925ab3c8dcea807b9d73bb9996f11b8
alvinchen1 <github.interest@alvinchen.net>
2019-02-23 15:24:31 -0500

    Update nyce-open-closed-sensor.groovy

--- devicetypes/smartthings/nyce-open-closed-sensor.src/nyce-open-closed-sensor.groovy
+++ devicetypes/smartthings/nyce-open-closed-sensor.src/nyce-open-closed-sensor.groovy
@@ -12,6 +12,8 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  Sourced from https://github.com/verbem/SmartThingsPublic/blob/master/devicetypes/smartthings/zwave-door-window-sensor.src/zwave-door-window-sensor.groovy
+ *
  *  Updates:
  *  -------
  *  02-23-2019 : Doing my own things with colors
